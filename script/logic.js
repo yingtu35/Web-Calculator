260067537da8cf27cd44a89b2f843fa2fa989510
yingtu35 <91551415+yingtu35@users.noreply.github.com>
2023-01-01 14:38:52 +0800

    v2.0 upload
    the second version of the calculator
    +/-, % buttons added
    calculation logic fixed
    dot button not implemented

--- script/logic.js
+++ script/logic.js
@@ -5,99 +5,156 @@
 /* v1.0: cannot handle parentheses, expression after equal is pressed, decimals */
 
 /* hold the user input expression */
-let expression = "0";
+let previous_expr = "";
+let current_expr = "0";
+let temp_expr = "0";
+let operator = "";
+let new_number = true;
 let result_exp = false;
 
 /* validate the expression */
 const re = /(?:(?:^|[-+_*/])(?:\s*-?\d+(\.\d+)?(?:[eE][+-]?\d+)?\s*))+$/;
 
-/* properties for the expression shown on the screen */
-font = "bold 70px Arial";
-font_color = "black";
+// /* properties for the expression shown on the screen */
+// font = "bold 70px Arial";
+// font_color = "black";
 
-/* logic of the calculator */
-window.onload = function () {
-    const canvas = document.getElementById("canvas");
-    width = canvas.width;
-    height = canvas.height;
+// /* logic of the calculator */
+// window.onload = function () {
+//     const canvas = document.getElementById("canvas");
+//     width = canvas.width;
+//     height = canvas.height;
 
-    let context =canvas.getContext("2d");
-    context.font = font;
-    context.fillStyle=font_color;
+//     let context =canvas.getContext("2d");
+//     context.font = font;
+//     context.fillStyle=font_color;
 
-    /* draw the expression on the canvas */
-    function draw(currentTime) {
-        context.clearRect(0,0,width,height);
+//     /* draw the expression on the canvas */
+//     function draw(currentTime) {
+//         context.clearRect(0,0,width,height);
 		
-        context.beginPath();
-        context.fillText(expression,0,65);
-        // context.fillText(expression,width- 2 - 39*(expression.length),65);
+//         context.beginPath();
+//         context.fillText(current_expr,0,65);
+//         // context.fillText(current_expr,width- 2 - 39*(current_expr.length),65);
 
-        window.requestAnimationFrame(draw);
-   }
-    /* handle all button click events */
+//         window.requestAnimationFrame(draw);
+//    }
+//     /* handle all button click events */
 
-    window.requestAnimationFrame(draw);
+//     window.requestAnimationFrame(draw);
 
+// }
+function display() {
+    current_expr = current_expr.slice(0,13);
+    $("#screen p").html(current_expr);
 }
-
 function clearExp() {
-    expression = "0";
+    previous_expr = "";
+    current_expr = "0";
+    temp_expr = "";
+    operator = "";
+    new_number = true;
     result_exp = false;
+    display();
 }
 
 function addExp(parameter) {
     switch (parameter) {
-        case 'left':
-            if (result_exp) {
-                expression = "(";
+        case 'minus':
+            if (current_expr.charAt(0) === "-") {
+                current_expr = current_expr.slice(1,);
             }else{
-                expression = expression === "0"? "(": (expression + "(");
+                current_expr = "-" + current_expr;
             }
+            new_number = false;
             result_exp = false;
+            display();
             break;
-        case 'right':
-            if (result_exp) {
-                expression = ")";
-            }else{
-                expression = expression === "0"? ")": (expression + ")");
-            }
+        case 'percent':
+            current_expr = 0.01 * Number(current_expr);
+            current_expr = current_expr.toString();
             break;
         case '+':
         case '-':
         case '*':
         case '/':
-            expression += parameter;
+            if (!new_number) {
+                // var temp = current_expr;
+                current_expr = calExp(previous_expr, current_expr, operator);
+                // alert(current_expr);
+                // operator = parameter;
+                previous_expr = current_expr;
+                // alert(current_expr);
+                
+                // current_expr += parameter;
+                new_number = true;
+                temp_expr = "";
+            }
+            operator = parameter;
             result_exp = false;
             break;
         default:
             if (result_exp){
-                expression = parameter;
-            }else{
-                expression = expression === "0"? parameter: (expression + parameter);
+                previous_expr = "0";
+                temp_expr = "";
+                result_exp = false;
             }
-            resule_exp = false;
+            // }else{
+            //     previous_expr = current_expr;
+            // }
+            if (current_expr === "0" || new_number){
+                current_expr = parameter;
+                new_number = false;
+            }
+            else if (current_expr === "-0") {
+                current_expr = "-" + parameter;
+            }
+            else{
+                current_expr += parameter;
+            }
+            temp_expr += parameter;
+            result_exp = false;
             break;
     }
+    display();
 }
 
 function subExp() {
-    if (expression.length !== 1) {
-        expression = expression.slice(0, -1);
+    var expr = current_expr.toString();
+    if (expr.length !== 1) {
+        expr = expr.slice(0, -1);
     }else {
-        expression = "0";
+        expr = "0";
     }
+    current_expr = expr;
+    display();
 }
 
-function calExp() {
-    result_exp = true;
-
-    if (isValidMathExpression(expression)){
-        var func = new Function("return " + expression);
-        expression = func();
+function equalExp() {
+    if (result_exp){
+        // alert(current_expr);
+        // alert(temp_expr);
+        current_expr = calExp(current_expr, temp_expr, operator);
     }else{
-        expression = "Error";
+        current_expr = calExp(previous_expr, current_expr, operator);
+        result_exp = true;
     }
+    previous_expr = current_expr;
+    new_number = true;
+    display();
+}
+
+function calExp(expr1, expr2, op) {
+    // result_exp = true;
+    var expr = expr1 + op + expr2;
+    
+    if (isValidMathExpression(expr)){
+        var func = new Function("return " + expr);
+        expr = func();
+    }else{
+        expr = "Error";
+    }
+    return expr.toString();
 }
 
 function isValidMathExpression(expr){
